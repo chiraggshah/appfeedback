b2bbc7dfb058ff40c04b83c0a295fd0749ffc33b
Chirag Shah <chirag@bigbinary.com>
2021-08-18 02:32:58 +0530

    Show actions only if user is owner of feedback

--- components/ui/FeedbackList.js
+++ components/ui/FeedbackList.js
@@ -1,21 +1,27 @@
+import Cookies from "js-cookie";
+
 const FeedbackList = ({ feedbacks }) =>
   feedbacks.map((item) => <Feedback key={item.id} {...item} />);
 
-const Feedback = ({ title, vote_count, description }) => (
-  <div className="flex flex-row border rounded-md my-4 p-5 bg-white justify-between shadow-sm">
-    <div className="flex flex-row">
-      <div className="flex flex-shrink-0 flex-col border rounded-md h-12 w-12 bg-indigo-600 items-center justify-center text-white">
-        <CaretUp />
-        {vote_count}
+const Feedback = ({ title, vote_count, description, id, user_id, ...rest }) => {
+  const currentUserId = Cookies.get("token");
+
+  return (
+    <div className="flex flex-row border rounded-md my-4 p-5 bg-white justify-between shadow-sm">
+      <div className="flex flex-row">
+        <div className="flex flex-shrink-0 flex-col border rounded-md h-12 w-12 bg-indigo-600 items-center justify-center text-white">
+          <CaretUp />
+          {vote_count}
+        </div>
+        <div className="flex flex-col px-5">
+          <div className="font-medium">{title}</div>
+          <div className="font-light flex-wrap text-sm mt-1">{description}</div>
+        </div>
       </div>
-      <div className="flex flex-col px-5">
-        <div className="font-medium">{title}</div>
-        <div className="font-light flex-wrap text-sm mt-1">{description}</div>
-      </div>
+      <div className="flex">{currentUserId === user_id ? "..." : ""}</div>
     </div>
-    <div className="flex">...</div>
-  </div>
-);
+  );
+};
 
 const CaretUp = () => (
   <svg
